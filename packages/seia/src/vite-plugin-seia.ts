dfa9191f8b91f555421f767d1125fec8257eb72e
Abiria <abiria.dev@gmail.com>
2024-05-23 06:56:21 +0900

    chore: remove paranthesis

--- packages/seia/src/vite-plugin-seia.ts
+++ packages/seia/src/vite-plugin-seia.ts
@@ -10,14 +10,14 @@ export const seia = (): Plugin => {
 		async transform(code, id) {
 			if (!id.endsWith('.tsx')) return null
 
-			const { source } = (await transformSource(
+			const { source } = await transformSource(
 				code,
 				{
 					format: 'module',
 					url: id,
 				},
 				async (source: string) => ({ source }),
-			)) as { source: string }
+			)
 
 			return source.replace(
 				'react-server-dom-webpack/server',
