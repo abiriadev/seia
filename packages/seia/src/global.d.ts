9a627565fb6ad0ce01c70ddc574967c26f66ed86
Abiria <abiria.dev@gmail.com>
2024-05-23 03:28:24 +0900

    feat: add types from RSDW

--- packages/seia/src/global.d.ts
+++ packages/seia/src/global.d.ts
@@ -18,3 +18,39 @@ declare module 'react-server-dom-webpack/node-loader' {
 		defaultTransformSource: TransformSourceFunction,
 	): Promise<{ source: Source }>
 }
+
+declare module 'react-server-dom-webpack/server.edge' {
+	type ImportManifestEntry = {
+		id: string
+		chunks: Array<string>
+		name: string
+	}
+
+	type ClientReferenceManifestEntry = ImportManifestEntry
+
+	export type ClientManifest = {
+		[id: string]: ClientReferenceManifestEntry
+	}
+
+	interface TemporaryReference<T> {}
+
+	type TemporaryReferenceSet = WeakMap<
+		TemporaryReference<any>,
+		string
+	>
+
+	type Options = {
+		environmentName?: string
+		identifierPrefix?: string
+		signal?: any // AbortSignal
+		temporaryReferences?: TemporaryReferenceSet
+		onError?: (error: mixed) => void
+		onPostpone?: (reason: string) => void
+	}
+
+	export function renderToReadableStream(
+		model: any, // ReactClientValue
+		webpackMap: ClientManifest,
+		options?: Options,
+	): ReadableStream
+}
