99e7c4b1f6fadbcbadcbc5c932f66bfe4fa403b6
Abiria <abiria.dev@gmail.com>
2024-05-23 05:51:07 +0900

    refactor: make render to return DOM instead of stream

--- packages/seia/src/renderer.ts
+++ packages/seia/src/renderer.ts
@@ -1,15 +1,17 @@
+import { createFromReadableStream } from 'react-server-dom-webpack/client.edge'
 import { Worker } from 'node:worker_threads'
 import { fileURLToPath } from 'node:url'
 import { join } from 'node:path'
+import { ReactNode } from 'react'
 
 const workerUrl = join(
 	fileURLToPath(new URL('.', import.meta.url)),
 	'worker.js',
 )
 
-export const renderRsc = async (
+const execWorker = async (
 	componentUrl: string,
-): Promise<string> =>
+): Promise<[Worker, ReadableStream]> =>
 	new Promise((resolve, reject) => {
 		const worker = new Worker(workerUrl, {
 			workerData: {
@@ -18,6 +20,33 @@ export const renderRsc = async (
 			execArgv: ['-C', 'react-server'],
 		})
 
-		worker.on('error', reject)
-		worker.on('message', resolve)
+		worker.on('error', async e => {
+			await worker.terminate()
+			reject(e)
+		})
+		worker.on('message', rs => resolve([worker, rs]))
 	})
+
+export const renderRscDom = async (
+	componentUrl: string,
+): Promise<ReactNode> => {
+	const [worker, rs] = await execWorker(componentUrl)
+
+	const dom = await createFromReadableStream(rs, {
+		ssrManifest: {
+			moduleMap: new Proxy(
+				{},
+				{
+					get(target, p, receiver) {
+						console.log(target)
+					},
+				},
+			),
+			moduleLoading: null,
+		},
+	})
+
+	await worker.terminate()
+
+	return dom
+}
