1336691a464d0a936b610aa3231dec4e726ad00e
Abiria <abiria.dev@gmail.com>
2024-05-28 08:08:27 +0900

    feat: extract virtual module info from absolute path

--- packages/seia/src/plugins/inject-client.ts
+++ packages/seia/src/plugins/inject-client.ts
@@ -4,11 +4,12 @@ export const injectClient = (): Plugin => {
 	return {
 		name: 'seia:inject-client',
 		resolveId(source) {
-			if (source === 'client.js') return '\0client.js'
+			if (source.endsWith('\0client.js'))
+				return '\0client.js'
 		},
 		load(id) {
 			if (id === '\0client.js')
-				return `import 'seia/client'`
+				return `import 'seia-js/client'`
 		},
 	}
 }
