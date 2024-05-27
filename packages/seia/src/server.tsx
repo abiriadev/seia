1582cabebbc1ea80c9e8811b7e58c19afc3a1e86
Abiria <abiria.dev@gmail.com>
2024-05-28 07:31:30 +0900

    feat: add endpoint for static file serving

--- packages/seia/src/server.tsx
+++ packages/seia/src/server.tsx
@@ -1,19 +1,45 @@
 import './webpack-global.js'
 import { Hono } from 'hono'
 import { serve as nodeServe } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
 import { cwd } from 'node:process'
 import { join } from 'node:path'
 import { logger } from 'hono/logger'
-import { renderRscDom } from './renderer.js'
+import {
+	renderRscDom,
+	renderRscPayloadStream,
+} from './renderer.js'
 import { renderToReadableStream } from 'react-dom/server.edge'
 
 const app = new Hono()
 
 app.use(logger())
 
+app.use(
+	'/@seia/*',
+	serveStatic({
+		root: './dist',
+		rewriteRequestPath: path =>
+			path.replace(/^\/@seia/, ''),
+	}),
+)
+
+app.get('/@seia', async c => {
+	const entryFile = join(cwd(), './dist/rsc/App.js#App')
+
+	const [worker, stream] =
+		await renderRscPayloadStream(entryFile)
+
+	const __rsc = await new Response(stream).text()
+
+	await worker.terminate()
+
+	return c.text(__rsc)
+})
+
 /** @jsxImportSource hono/jsx */
 app.get('/', async c => {
-	const entryFile = join(cwd(), './dist/App.js#App')
+	const entryFile = join(cwd(), './dist/rsc/App.js#App')
 
 	const [worker, dom] = await renderRscDom(entryFile)
 
@@ -33,6 +59,10 @@ app.get('/', async c => {
 						content="width=device-width, initial-scale=1.0"
 					/>
 					<title>Seia SSR</title>
+					<script
+						type="module"
+						src="/@seia/client/"
+					/>
 				</head>
 				<body>
 					<div
