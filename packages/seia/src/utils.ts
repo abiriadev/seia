86843900cd268258536f14be7a022f5780f9bedd
Abiria <abiria.dev@gmail.com>
2024-05-28 17:19:43 +0900

    feat: add a util function to change file extension

--- packages/seia/src/utils.ts
+++ packages/seia/src/utils.ts
@@ -1,5 +1,16 @@
+import { format, parse } from 'node:path'
+
 export const trimPrefix = (
 	str: string,
 	prefix: string,
 ): string =>
 	str.startsWith(prefix) ? str.slice(prefix.length) : str
+
+export const changeExtension = (
+	path: string,
+	ext: string,
+): string =>
+	format({
+		...parse(path),
+		ext,
+	})
