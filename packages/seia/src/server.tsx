c081ab605c6774cb198b0b30f1b0669c80129a59
Abiria <abiria.dev@gmail.com>
2024-05-28 16:58:07 +0900

    refactor: move hono code into callback to configure server with config

--- packages/seia/src/server.tsx
+++ packages/seia/src/server.tsx
@@ -12,73 +12,85 @@ import {
 import { renderToReadableStream } from 'react-dom/server.edge'
 import { ResolvedSeiaConfig } from './config.js'
 
-const app = new Hono()
-
-app.use(logger())
-
-app.use(
-	'/@seia/*',
-	serveStatic({
-		root: './dist',
-		rewriteRequestPath: path =>
-			path.replace(/^\/@seia/, ''),
-	}),
-)
-
-app.get('/@seia', async c => {
-	const entryFile = join(cwd(), './dist/rsc/App.js#App')
-
-	const [worker, stream] =
-		await renderRscPayloadStream(entryFile)
-
-	const __rsc = await new Response(stream).text()
-
-	await worker.terminate()
-
-	return c.text(__rsc)
-})
-
-/** @jsxImportSource hono/jsx */
-app.get('/', async c => {
-	const entryFile = join(cwd(), './dist/rsc/App.js#App')
-
-	const [worker, dom] = await renderRscDom(entryFile)
-
-	const ren = await renderToReadableStream(dom)
-
-	const __html = await new Response(ren).text()
-
-	await worker.terminate()
-
-	return c.html(
-		(
-			<html>
-				<head>
-					<meta charSet="UTF-8" />
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1.0"
-					/>
-					<title>Seia SSR</title>
-					<script
-						type="module"
-						src="/@seia/client.js"
-					/>
-				</head>
-				<body>
-					<div
-						id="root"
-						dangerouslySetInnerHTML={{ __html }}
-					/>
-				</body>
-			</html>
-		).toString(),
-	)
-})
-
 export const serve = async ({
+	root,
+	dist,
 	serve: { port },
 }: ResolvedSeiaConfig) => {
+	const app = new Hono()
+
+	app.use(logger())
+
+	app.use(
+		'/@seia/*',
+		serveStatic({
+			root: dist,
+			rewriteRequestPath: path =>
+				path.replace(/^\/@seia/, ''),
+		}),
+	)
+
+	app.get('/@seia', async c => {
+		const entryFile = join(
+			root,
+			dist,
+			'./rsc/App.js#App',
+		)
+
+		const [worker, stream] =
+			await renderRscPayloadStream(entryFile)
+
+		const __rsc = await new Response(stream).text()
+
+		await worker.terminate()
+
+		return c.text(__rsc)
+	})
+
+	/** @jsxImportSource hono/jsx */
+	app.get('/', async c => {
+		const entryFile = join(
+			root,
+			dist,
+			'./rsc/App.js#App',
+		)
+
+		const [worker, dom] = await renderRscDom(entryFile)
+
+		const ren = await renderToReadableStream(dom)
+
+		const __html = await new Response(ren).text()
+
+		await worker.terminate()
+
+		return c.html(
+			(
+				<html>
+					<head>
+						<meta charSet="UTF-8" />
+						<meta
+							name="viewport"
+							content="width=device-width, initial-scale=1.0"
+						/>
+						<title>Seia SSR</title>
+						<script
+							type="module"
+							src="/@seia/client.js"
+						/>
+					</head>
+					<body>
+						<div
+							id="root"
+							dangerouslySetInnerHTML={{
+								__html,
+							}}
+						/>
+					</body>
+				</html>
+			).toString(),
+		)
+	})
+
 	console.log(`http://localhost:${port}`)
 
 	nodeServe({
