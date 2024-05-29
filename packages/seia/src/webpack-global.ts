93aa9ac5d14ac705b0d6ea298b05ec935c62bae3
Abiria <abiria.dev@gmail.com>
2024-05-29 23:26:10 +0900

    chore: remove unused lint disable comment

--- packages/seia/src/webpack-global.ts
+++ packages/seia/src/webpack-global.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
 globalThis.__webpack_module_loading__ ||= new Map<string, unknown>()
 globalThis.__webpack_module_cache__ ||= new Map<string, unknown>()
 globalThis.__webpack_chunk_load__ ||= async (id: string) =>
