0d72907c09eec905f1e145a96a8f5625ab7cb546
Abiria <abiria.dev@gmail.com>
2024-05-30 00:01:03 +0900

    lint: apply lint suggestion

--- packages/seia/src/utils.ts
+++ packages/seia/src/utils.ts
@@ -1,13 +1,13 @@
-export const trimPrefix = (str: string, prefix: string): string =>
-	str.startsWith(prefix) ? str.slice(prefix.length) : str
+export const trimPrefix = (path: string, prefix: string): string =>
+	path.startsWith(prefix) ? path.slice(prefix.length) : path
 
 export const isObject = (value: unknown): value is Record<string, unknown> =>
 	Object.prototype.toString.call(value) === '[object Object]'
 
-// valid JavaScript identifier
+// Valid JavaScript identifier
 export type Anchor = string
 
-// resolved id with anchor
+// Resolved id with anchor
 export type AnchorId = string
 
 export const parseAnchorId = (
@@ -20,8 +20,8 @@ export const parseAnchorId = (
 	return i === -1
 		? { path: id, anchor: null }
 		: {
-				path: id.substring(0, i),
-				anchor: id.substring(i + 1),
+				path: id.slice(0, Math.max(0, i)),
+				anchor: id.slice(Math.max(0, i + 1)),
 			}
 }
 
@@ -31,10 +31,10 @@ export const mustParseAnchorId = (
 	path: string
 	anchor: Anchor
 } => {
-	const res = parseAnchorId(id)
-	if (res.anchor === null)
+	const parsed = parseAnchorId(id)
+	if (parsed.anchor === null)
 		throw new Error(
 			`Can't extract anchor information from the given resolveId: ${id}`,
 		)
-	return res as { path: string; anchor: Anchor }
+	return parsed as { path: string; anchor: Anchor }
 }
