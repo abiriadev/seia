a6c22f0ff502a18ad88770ed21d1b19fa61938fa
Abiria <abiria.dev@gmail.com>
2024-05-29 11:27:00 +0900

    feat: apply ast generation

--- packages/seia/src/plugins/inject-client.ts
+++ packages/seia/src/plugins/inject-client.ts
@@ -2,7 +2,13 @@ import { type Plugin } from 'vite'
 import { relative } from 'node:path'
 import sum from 'hash-sum'
 import { ResolvedSeiaConfig } from '../config.js'
-import { changeExtension, trimPrefix } from '../utils.js'
+import { changeExtension } from '../utils.js'
+import type {
+	ImportDeclaration,
+	Program,
+	Property,
+} from 'estree'
+import { generate } from 'astring'
 
 export interface Options {
 	clientBoundaries: Array<string>
@@ -11,9 +17,7 @@ export interface Options {
 
 export const injectClient = ({
 	clientBoundaries,
-	config: {
-		paths: { src },
-	},
+	config,
 }: Options): Plugin => {
 	return {
 		name: 'seia:inject-client',
@@ -23,33 +27,150 @@ export const injectClient = ({
 		},
 		load(id) {
 			if (id === '\0client.js') {
-				const modules = clientBoundaries.map(
-					path => ['_' + sum(path), path],
+				const ast = injectManifest(
+					clientBoundaries.map(path => [
+						'_' + sum(path),
+						path,
+					]),
+					config,
 				)
 
-				const code = `
-import { initClientModuleMap, run } from 'seia-js/client'
+				const code = generate(ast, {})
 
-${modules
-	.map(([id, path]) => `import * as ${id} from '${path}'`)
-	.join('\n')}
-
-const clientModuleMap = {
-${modules
-	.map(
-		([id, path]) =>
-			`'${'./' + changeExtension(relative(src, path), '.js')}': ${id},`,
-	)
-	.join('\n')}
-}
-
-initClientModuleMap(clientModuleMap)
-
-run()
-			`
-
-				return code
+				return {
+					code,
+					ast,
+				}
 			}
 		},
 	}
 }
+
+const injectManifest = (
+	manifestArrary: Array<[string, string]>,
+	{ paths: { src } }: ResolvedSeiaConfig,
+): Program => ({
+	type: 'Program',
+	sourceType: 'module',
+	body: [
+		{
+			type: 'ImportDeclaration',
+			specifiers: [
+				{
+					type: 'ImportSpecifier',
+					local: {
+						type: 'Identifier',
+						name: 'initClientModuleMap',
+					},
+					imported: {
+						type: 'Identifier',
+						name: 'initClientModuleMap',
+					},
+				},
+				{
+					type: 'ImportSpecifier',
+					local: {
+						type: 'Identifier',
+						name: 'run',
+					},
+					imported: {
+						type: 'Identifier',
+						name: 'run',
+					},
+				},
+			],
+			source: {
+				type: 'Literal',
+				value: 'seia-js/client',
+			},
+		},
+		...manifestArrary.map(
+			([id, path]): ImportDeclaration => ({
+				type: 'ImportDeclaration',
+				specifiers: [
+					{
+						type: 'ImportNamespaceSpecifier',
+						local: {
+							type: 'Identifier',
+							name: id,
+						},
+					},
+				],
+				source: {
+					type: 'Literal',
+					value: path,
+				},
+			}),
+		),
+		{
+			type: 'VariableDeclaration',
+			declarations: [
+				{
+					type: 'VariableDeclarator',
+					id: {
+						type: 'Identifier',
+						name: 'clientModuleMap',
+					},
+					init: {
+						type: 'ObjectExpression',
+						properties: manifestArrary.map(
+							([id, path]): Property => ({
+								type: 'Property',
+								key: {
+									type: 'Literal',
+									value:
+										'./' +
+										changeExtension(
+											relative(
+												src,
+												path,
+											),
+											'.js',
+										),
+								},
+								value: {
+									type: 'Identifier',
+									name: id,
+								},
+								kind: 'init',
+								method: false,
+								shorthand: false,
+								computed: false,
+							}),
+						),
+					},
+				},
+			],
+			kind: 'const',
+		},
+		{
+			type: 'ExpressionStatement',
+			expression: {
+				type: 'CallExpression',
+				callee: {
+					type: 'Identifier',
+					name: 'initClientModuleMap',
+				},
+				arguments: [
+					{
+						type: 'Identifier',
+						name: 'clientModuleMap',
+					},
+				],
+				optional: false,
+			},
+		},
+		{
+			type: 'ExpressionStatement',
+			expression: {
+				type: 'CallExpression',
+				callee: {
+					type: 'Identifier',
+					name: 'run',
+				},
+				arguments: [],
+				optional: false,
+			},
+		},
+	],
+})
