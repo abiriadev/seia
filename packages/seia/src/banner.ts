f5a96738c5dfe29a973c2ac2b7a362d4ccf1c89c
Abiria <abiria.dev@gmail.com>
2024-05-29 14:01:21 +0900

    feat: move code to banner.ts

---
+++ packages/seia/src/banner.ts
@@ -0,0 +1,38 @@
+import chalk from 'chalk'
+import { version } from './package.js'
+import { ResolvedSeiaConfig } from './config.js'
+
+const seiaBanner = `  "m             #  m    mmmmm
+m  #mmm""#     m"    """"m  m"
+ ""#   m"  mm""m         #""  
+   #  "        #        m"    
+   "mmmmm      #       m"     `
+
+const seiaColor = '#fab359'
+
+const seiaChalk = chalk.hex(seiaColor)
+const seiaBgChalk = chalk.bgHex(seiaColor)
+
+const indent = '  '
+
+export const makeBanner = ({
+	serve: { port },
+}: ResolvedSeiaConfig) => {
+	return `\n${seiaChalk(
+		seiaBanner
+			.split('\n')
+			.map(line => `${indent}${line}`)
+			.join('\n'),
+	)}\n\n${indent}${seiaBgChalk.black(
+		' SEIA.js ',
+	)} ${seiaChalk(`v${version}`)}  ${chalk.dim(
+		`ready in ${chalk.bold(
+			Math.ceil(
+				performance.now() -
+					globalThis.__SEIA_START_TIME,
+			),
+		)} ms`,
+	)}\n\n${indent}${chalk.green('➜')} ${chalk.bold(
+		'Local',
+	)} \t${chalk.blue(`http://localhost:${port}`)}`
+}
