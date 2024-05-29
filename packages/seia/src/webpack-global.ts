cb361981c7cee408dcdca8f33fde05f7c7040a89
Abiria <abiria.dev@gmail.com>
2024-05-29 21:13:42 +0900

    lint: disable eslint for naming convention

--- packages/seia/src/webpack-global.ts
+++ packages/seia/src/webpack-global.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 globalThis.__webpack_module_loading__ ||= new Map<string, unknown>()
 globalThis.__webpack_module_cache__ ||= new Map<string, unknown>()
 globalThis.__webpack_chunk_load__ ||= async (id: string) =>
