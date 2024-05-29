c84bb0d57e65c986fd096111398291722045b72f
Abiria <abiria.dev@gmail.com>
2024-05-29 20:25:08 +0900

    fix: lint prevent-abbr

--- packages/seia/src/utils-path.ts
+++ packages/seia/src/utils-path.ts
@@ -1,5 +1,5 @@
 // Manual code splitting because of rollup's limitations
 import { basename, dirname, extname, join } from 'node:path'
 
-export const changeExtension = (path: string, ext: string): string =>
-	join(dirname(path), basename(path, extname(path)) + ext)
+export const changeExtension = (path: string, extension: string): string =>
+	join(dirname(path), basename(path, extname(path)) + extension)
