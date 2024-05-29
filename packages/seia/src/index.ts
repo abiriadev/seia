7473b82f5fb2654955db542f75c6ae00c8f78a14
Abiria <abiria.dev@gmail.com>
2024-05-30 02:27:14 +0900

    feat: make it public api, add entrypoint

---
+++ packages/seia/src/index.ts
@@ -0,0 +1,6 @@
+// Seia public api
+
+export { SeiaConfig, mergeSeiaConfig } from './config.js'
+export { build } from './build.js'
+export { serve } from './server.js'
+export { version } from './package.js'
