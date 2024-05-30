fd0e5ac40c1bd833a77fdd6bd9f1cd0cd494b651
Abiria <abiria.dev@gmail.com>
2024-05-30 09:33:56 +0900

    build: setup tailwind

---
+++ examples/blue-archive-students/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#128afa',
+			},
+			fontFamily: {
+				'blue-archive': ['GlowSansSC-Normal-Heavy_diff'],
+			},
+		},
+	},
+	plugins: [],
+}
