1056f753944e8468db88ccb041cf3c0fab75227f
Abiria <abiria.dev@gmail.com>
2024-05-30 17:59:09 +0900

    feat: this is how server component works

--- examples/blue-archive-students/src/Header.tsx
+++ examples/blue-archive-students/src/Header.tsx
@@ -1,10 +1,18 @@
-export const Header = () => {
+// This is server component.
+// It reads(!) `package.json` with native `node:fs` module.
+import { readFile } from 'node:fs/promises'
+
+export const Header = async () => {
+	const { version } = JSON.parse(
+		(await readFile('./package.json')).toString(),
+	) as { version: string }
+
 	return (
 		<header className="flex flex-col justify-center items-center pt-28 pb-12 px-8">
 			<h1 className="text-5xl font-extrabold font-blue-archive italic mb-4 text-center">
 				<span className="text-primary">Blue Archive</span> Students
 			</h1>
-			<p className="font-blue-archive text-2xl">v1.0.0</p>
+			<p className="font-blue-archive text-2xl">v{version}</p>
 		</header>
 	)
 }
