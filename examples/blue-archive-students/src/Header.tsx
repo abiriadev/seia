06f5a278ce573d090ed90a4345de60ef963748cf
Abiria <abiria.dev@gmail.com>
2024-05-31 15:08:54 +0900

    feat: concat 'Blue' and 'Archive', while allowing browser to break them

--- examples/blue-archive-students/src/Header.tsx
+++ examples/blue-archive-students/src/Header.tsx
@@ -10,7 +10,13 @@ export const Header = async () => {
 	return (
 		<header className="flex flex-col justify-center items-center pt-28 pb-12 px-8">
 			<h1 className="text-5xl font-extrabold font-blue-archive italic mb-4 text-center">
-				<span className="text-primary">Blue Archive</span> Students
+				<span className="text-primary">
+					Blue
+					{/* https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-wbr-element */}
+					<wbr />
+					Archive
+				</span>{' '}
+				Students
 			</h1>
 			<p className="font-blue-archive text-2xl">v{version}</p>
 		</header>
