1b7dbd0d7f5957813496e162a15fd97a294a9581
Ji Yong, Kim <nemo@qroffle.com>
2024-06-02 22:33:35 +0900

    chore: fix typo

--- packages/seia/src/process-polyfill.ts
+++ packages/seia/src/process-polyfill.ts
@@ -1,8 +1,8 @@
-// NOTE: typescript declare statement is global, so we should avoid redeclaring global `process` here, thus used `as any`
+// NOTE: typescript declare statement is global, so we should avoid re-declaring global `process` here, thus used `as any`
 // it will now work well with `@types/node`, but of course this is hack. not a great idea.
 ;(globalThis as any).process = {
 	env: {
 		// eslint-disable-next-line @typescript-eslint/naming-convention
-		NODE_ENV: 'prodection',
+		NODE_ENV: 'production',
 	},
 }
